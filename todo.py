e61cdef50d45469d8f96eb955feb943266eb3e2b
amon-sudo <amonogino@gmail.com>
2026-03-02 21:22:10 +0300

    we are almost there

--- todo.py
+++ todo.py
@@ -14,7 +14,7 @@ def load_task():
       with open(file_name, "r") as file:
          return json.load(file)
     except:
-     return print("Failed to save .")
+        print("Failed to save .")
     
     
     
@@ -25,34 +25,59 @@ def save_task(tasks):
          return json.dump(tasks, file)
     except:
      return {"tasks": []}
-def view_task():
-    pass
-def create_task():
-    pass
-def mark_task_complet():
-    pass
+def view_task(tasks):
+    tasks_list = tasks["tasks"]
+    if len(tasks_list) == 0:
+        print("No Tasks available.")
+    else:
+        print("Your todo list is: ")
+        for idx, task in enumerate(tasks_list):
+            status = "[Completed]" if task["completed"] else "[pending]"
+            print(f"{idx +1}.  {task['description']} | {status}")
+def create_task(tasks):
+    description = input("Enter your task: ")
+    if description:
+        tasks["tasks"].append({"description": description, "completed": False})
+        save_task(tasks)
+        print("Added Successfully")
+    else:
+        print("Description Cannot be empty")
+def mark_task_complete(tasks):
+    view_task(tasks)
+   
+    try:
+         task_number = int(input("Enter the task number to mark as completed: ").strip())
+         if 1 <= task_number <= len(tasks):
+            tasks["tasks"][task_number - 1]["completed"] = True
+            save_task(tasks)
+            print("Task marked as completed")
+         else:
+            print("Enter avalid number")
+    except:
+        print("Enter a number.")
+       
 
 def main():
-    save_task({"taska": ["what is saved"]})
     tasks = load_task()
-    print(tasks)
     
     while True:
         print("\nTo-Do List Manager")
         print("1. View Tasks")
         print("2. Add Task")
         print("3. Complete Task")
-        print("4. Print Task")
+        print("4. Exit")
         choice = input("Enter Your Choice: ").strip()
         
         if choice == "1":
-            view_task()
+            view_task(tasks)
         elif choice == "2":
-            create_task()
+            create_task(tasks)
         elif choice == "3":
-            mark_task_complet()
+            mark_task_complete(tasks)
         elif choice == "4":
+            
             print("Welcome Again!!")
+            break
         else:
             print("Please Enter avalid number. Try again!.")
     
